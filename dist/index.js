d8fd7c70598167218c51e66620cdef9816a7291a
Siddharth <siddharth.muscat@gmail.com>
2021-03-22 16:47:38 +0530

    added current karma in activity log

--- dist/index.js
+++ dist/index.js
@@ -793,7 +793,7 @@ const buildReadme = (prevReadmeContent, data) => {
   const plusSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#32CD32	" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>';
   parsedData.activity.forEach(act => {
     let template = [
-      "<ul>"," <li>",`   <h3>${act.time} ${plusSVG}${act.plus} ${minusSVG}${act.minus}</h3>`,"     <ul>"
+      "<ul>"," <li>",`   <h3>${act.time} ${plusSVG}${act.plus} ${minusSVG}${act.minus} karma (currently ${act.current} karma)</h3>`,"     <ul>"
     ];
 
     act.plusReasons.forEach(plusReason => template.push(`       <li>${plusSVG} ${plusReason}</li>`));
